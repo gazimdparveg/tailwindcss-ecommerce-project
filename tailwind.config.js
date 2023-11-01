2fea9edccad9c5708b56deb08b0092f27b0dcd36
gazimdparveg <storybook133@gmail.com>
2023-11-01 20:57:42 +0900

    day3

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,29 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+   sm:"480px",
+   md:"768px",
+   lg:"1024px"
+    },
     extend: {
       colors:{
-        mycolor:"#01A8F7"
+        mycolor:"#01A8F7",
+        textcolor: "#fff",
+        menucolor: "#1F1F1F",
       }
     },
+    container: {
+      center: true,
+      padding: {
+        default: '1rem',
+        sm: '1.5rem'
+      }
+    }, 
+   fontFamily: {
+    oswold:['Oswold', 'sans-serif,'],
+    dmsans:['DM Sans','sans-serif']
+   }    
   },
   plugins: [],
 }
